e198f983c03c06b15b21d25c890392012f3a1997
zhengnaying <55484284+zhengnaying@users.noreply.github.com>
2020-11-09 18:51:10 +0800

    1

--- app/src/main/java/com/example/intent/MainActivity.java
+++ app/src/main/java/com/example/intent/MainActivity.java
@@ -8,11 +8,12 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-   Button b1 = findViewById(R.id.b1);
+   Button b1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        b1=(Button)findViewById(R.id.b1);
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
